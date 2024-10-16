c99a887e8c761083463e27a394e28246fed9cb3a
mac <TheAlexeyShepard@gmail.com>
2024-10-16 14:29:44 +0300

    Вторая версия тест кейса

--- Task2/leave_request.py
+++ Task2/leave_request.py
@@ -11,19 +11,20 @@ def run(playwright: Playwright) -> None:
     page.locator("[id=\"\\34 704357\"] a").click()
     page.locator("mts-product-header-connection").get_by_text("Подключить").click()
     page.get_by_role("textbox", name="XXX XXX XX XX").click()
-    page.get_by_role("textbox", name="XXX XXX XX XX").fill("9186253419")
+    page.get_by_role("textbox", name="XXX XXX XX XX").fill("9186253496")
     page.get_by_role("textbox", name="Ваше имя").click()
-    page.get_by_role("textbox", name="Ваше имя").fill("Прохор")
+    page.get_by_role("textbox", name="Ваше имя").fill("Иосиф")
     page.get_by_role("button", name="Оставить заявку").click()
     actual_text = page.get_by_text("Заявка отправлена").inner_text()
 
     print(actual_text)
 
+    expect(page.get_by_text("Заявка отправлена")).to_be_visible()
     # ---------------------
     context.close()
     browser.close()
 
-    assert actual_text == 'Заявка отправлена'
+
 
 
 with sync_playwright() as playwright:
