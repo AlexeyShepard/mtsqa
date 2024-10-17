b334f4f344364131f3d795d8106a2ff9f12619f1
mac <TheAlexeyShepard@gmail.com>
2024-10-17 19:40:52 +0300

    Написал сервис для третьего задания

---
+++ Task3/README.md
@@ -0,0 +1,11 @@
+## КАК ЗАПУСТИТЬ СЕРВИС??
+
+```
+
+cd Task3
+
+fastapi dev service.py
+
+```
+
+Swagger: http://127.0.0.1:8000/docs 
---
+++ Task3/service.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+import random
+
+app = FastAPI()
+
+@app.post("/inverse")
+async def inverse(data: dict) -> JSONResponse:
+    inverse_data = {v: k for k, v in data.items()}
+    return JSONResponse(content=inverse_data, status_code=200)
+
+@app.get("/unstable")
+async def unstable() -> JSONResponse:
+
+    responses = [JSONResponse(content="НAPPY", status_code=200),
+                JSONResponse(content="UNHAPPY", status_code=400)]
+
+    response = random.choice(responses)
+
+    return response
