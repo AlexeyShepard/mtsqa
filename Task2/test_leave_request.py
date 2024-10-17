c8a0aed7fe428dfd67c44048a2bee833e9b21ff7
mac <TheAlexeyShepard@gmail.com>
2024-10-17 17:45:55 +0300

    Переделал позитивный тест

--- Task2/test_leave_request.py
+++ Task2/test_leave_request.py
@@ -1,56 +1,107 @@
+import random
+
 import pytest
 import allure
-from playwright.sync_api import Playwright, expect
+from pydantic import BaseModel
+from playwright.sync_api import Playwright, expect, Page
+
+class FormData(BaseModel):
+    number: str
+    name: str
+
+def random_number():
+    with allure.step('Генерируем случайный номер телефона'):
+        phone_number = '9'
+
+        for _ in range(9):
+            phone_number += str(random.randint(0, 9))
+
+        return phone_number
+
+def go_to_homepage(page):
+    with allure.step('Переходим на страницу mts.ru'):
+        page.goto("https://moskva.mts.ru/personal")
+
+def go_to_tariffs(page):
+    with allure.step('Нажать на кнопку "Тарифы"'):
+        page.locator("mts-ecosystem-navigation-widget a").filter(has_text="Тарифы").click()
+
+
+def go_to_tariff_description(page):
+    with allure.step('Нажать кнопку "Подробнее" на карточке "Тариф №1"'):
+        page.locator("[id=\"\\34 704357\"] a").click()
+
+def go_to_connect_the_tariff_form(page):
+    with allure.step('Нажать на кнопку "Подключить"'):
+        page.locator("mts-product-header-connection").get_by_text("Подключить").click()
+
+def fill_the_form(page, add_form_data):
+    with allure.step(f'Заполняем номер телефона, number: {add_form_data["number"]}'):
+        page.get_by_role("textbox", name="XXX XXX XX XX").click()
+        page.get_by_role("textbox", name="XXX XXX XX XX").fill(add_form_data['number'])
+    with allure.step(f'Заполняем имя клиента: {add_form_data["name"]}'):
+        page.get_by_role("textbox", name="Ваше имя").click()
+        page.get_by_role("textbox", name="Ваше имя").fill(add_form_data['name'])
+
+def send_data(page):
+    page.get_by_role("button", name="Оставить заявку").click()
 
 @pytest.fixture()
-def form_data():
-    return {
-        'number':'9186253416',
-        'name':'Алексей'
-    }
+def add_form_data():
+    return FormData(
+        number=random_number(),
+        name='Алексей'
+    ).model_dump()
 
-
-def browser_emulate(playwright, form_data):
+@pytest.fixture(scope="session")
+def context_browser(playwright: Playwright) -> Page:
     with allure.step('Создаем экземпляр браузера'):
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
-    with allure.step('Переходим на страницу mts.ru'):
-        page.goto("https://moskva.mts.ru/personal")
-    with allure.step('Переходим в раздел тарифов'):
-        page.locator("mts-ecosystem-navigation-widget a").filter(has_text="Тарифы").click()
-        page.locator("[id=\"\\34 704357\"] a").click()
-    with allure.step('Открываем форму отправки заявки на оформление тарифа'):
-        page.locator("mts-product-header-connection").get_by_text("Подключить").click()
-    with allure.step('Ожидаем, когда все элементы формы прогрузятся в DOM'):
-        page.wait_for_selector('div.mm-web-button__text:has-text("Оставить заявку")', state='attached')
-        page.wait_for_selector('input[placeholder="XXX XXX XX XX"]', state='attached')
-        page.wait_for_selector('input#username[placeholder="Ваше имя"]', state='attached')
-    with allure.step('Заполняем номер телефона'):
-        page.get_by_role("textbox", name="XXX XXX XX XX").click()
-        page.get_by_role("textbox", name="XXX XXX XX XX").fill(form_data['number'])
-    with allure.step('Заполняем имя клиента'):
-        page.get_by_role("textbox", name="Ваше имя").click()
-        page.get_by_role("textbox", name="Ваше имя").fill(form_data['name'])
-    with allure.step('Отправляем данные'):
-        page.get_by_role("button", name="Оставить заявку").click()
-    with allure.step('Ожидаем, что на странице куда нас перенаправит будет текст "Заявка отправлена" '):
-        expect(page.get_by_text("Заявка отправлена")).to_be_visible()
-
-        context.close()
-        browser.close()
-
+        return page
 
 @allure.epic('Оставить заявку на оформление тарифа')
-@allure.story('Пошагово переходим на форму заполнения данных и оправляем их')
-class TestLeaveRequest():
+@allure.story('Позитивные тесты')
+class TestLeaveRequestPositive():
     @staticmethod
-    @allure.title('Тест для отправления заявки с данными которых нету в базе данных')
-    def test_positive(playwright: Playwright, form_data) -> None:
-        browser_emulate(playwright, form_data)
+    @allure.title('Перейти на страницу https://moskva.mts.ru/personal')
+    def test_go_to_mts_page(context_browser):
+        go_to_homepage(context_browser)
+        expect(context_browser.get_by_text("Пополнение и оплата")).to_be_visible()
 
     @staticmethod
-    @allure.title('Тест для отправления заявки с данными, которые уже есть в базе данных')
-    def test_negative(playwright: Playwright, form_data) -> None:
-        browser_emulate(playwright, form_data)
+    @allure.title('Переходим в раздел тарифов')
+    def test_go_to_tariffs(context_browser):
+        go_to_tariffs(context_browser)
+        expect(context_browser.get_by_text("Тариф №1")).to_be_visible()
+
+    @staticmethod
+    @allure.title('Переход к тарифу №1')
+    def test_go_to_tariff_description(context_browser):
+        go_to_tariff_description(context_browser)
+        expected_result = 'Тариф с мобильной связью, домашним интернетом, ТВ и онлайн-кинотеатром KION'
+        expect(context_browser.get_by_text(expected_result)).to_be_visible()
+
+    @staticmethod
+    @allure.title('Переход к форме заполнения заявки')
+    def test_go_to_connect_the_tariff_form(context_browser):
+        go_to_connect_the_tariff_form(context_browser)
+        expect(context_browser.get_by_text("Заявка на подключение")).to_be_visible()
+
+    @staticmethod
+    @allure.title('Заполнение формы заявки')
+    def test_fill_the_form(context_browser, add_form_data):
+        fill_the_form(context_browser, add_form_data)
+        warnings = ["Заполните поле", "Укажите Ваше имя"]
+        expect(context_browser.get_by_text("Заявка на подключение")).not_to_have_text(warnings)
+
+    @staticmethod
+    @allure.title('Отправить заявку')
+    def test_send_the_data(context_browser):
+        send_data(context_browser)
+        expect(context_browser.get_by_text("Заявка отправлена")).to_be_visible()
+
+
+
 
