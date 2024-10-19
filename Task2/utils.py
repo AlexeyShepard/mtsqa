b90173a4013fad27bdf828e1a78a9f98665d770d
mac <TheAlexeyShepard@gmail.com>
2024-10-19 15:05:31 +0300

    Изменил генерацию номера

--- Task2/utils.py
+++ Task2/utils.py
@@ -2,9 +2,9 @@ import random
 
 # Генерация случайного номера для формы
 def random_number():
-        phone_number = '9'
+        phone_number = '901'
 
-        for _ in range(9):
+        for _ in range(7):
             phone_number += str(random.randint(0, 9))
 
         return phone_number
